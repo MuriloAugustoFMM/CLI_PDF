9d7f2dffb7bb7e7c4f4a112a022a4af19270b79f
Suporte Adm02.ap <suporteadm02.ap@masterlocacao.local>
2026-08-28 14:40:20 -0300

    Adicionando django na aplicacao

--- CLI/run.py
+++
@@ -1,9 +0,0 @@
-from .interface_controller import InterfaceController
-
-
-interface_controller = InterfaceController()
-#LOOP PRINCIPAL DA INTERFACE
-
-while True:
-    interface_controller.start()
-    
