5f37de5d53d8ec42f0827036331c9e26b0e6f292
Suporte Adm02.ap <suporteadm02.ap@masterlocacao.local>
2026-09-01 17:01:46 -0300

    modelando o banco de dados

--- main/models.py
+++ main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Checklist(models.Model):
+    STATUS_OPTIONS =[
+        ''
+    ]
+    title = models.CharField(max_length=40)
+    date = models.DateField(auto_now_add=True)
+    status = models.CharField(max_lenght=40)
+    
+
+class Item(models.Model):
+    question = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+
+class ItemChecklist(models.Model):
+    item_id = models.ForeignKey(to=Item,on_delete=models.CASCADE)
+    checklist_id = models.ForeignKey(to=Checklist,on_delete=models.CASCADE)
+    optional_image = models.BooleanField(default=False)
+
+class ImagensChecklist(models.Model):
+    fk_checklist = models.ForeignKey(to=Checklist,on_delete=models.CASCADE,blank=True)
+    fk_item = models.ForeignKey(to=Item,on_delete=models.CASCADE)
+
+
